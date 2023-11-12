fa0e2d8b68bd0020fa33f8473957270546bedc45
BuildTools <unconfigured@null.spigotmc.org>
2023-11-12 18:22:49 +0700

    initial commit

---
+++ app/Models/RiwayatObat.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RiwayatObat extends Model
+{
+    protected $table="riwayat_obat";
+    use HasFactory;
+}
---
+++ app/Models/riwayat_penyakit_alergi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class riwayat_penyakit_alergi extends Model
+{
+    protected $table="riwayat_penyakit_alergi";
+    use HasFactory;
+}
