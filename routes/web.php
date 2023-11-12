569b72b75ce02e2fe23bf66b5fea59cefd075c94
Gabriel Ivan Setyaputra <gabriel.setyaputra@grabtaxi.com>
2023-11-12 23:16:03 +0700

    default to slash

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::group(['prefix' => '/admin'], function () {
+Route::group(['prefix' => '/'], function () {
     Voyager::routes();
 });
