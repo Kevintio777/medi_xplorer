25a607443e1b1c524ab9c3d2959bf85cb5689203
Gabriel Ivan Setyaputra <gabriel.setyaputra@grabtaxi.com>
2023-11-12 21:47:17 +0700

    default routes voyage

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Voyager::routes();
 });
 
 Route::group(['prefix' => 'admin'], function () {
