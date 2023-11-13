e84ebb305d4e1d8e2698c3929e52b0fbf716e779
Gabriel Ivan Setyaputra <gabriel.setyaputra@grabtaxi.com>
2023-11-14 00:44:49 +0700

    update riwayat penyakit and alergi to be seperated

---
+++ app/Models/RiwayatAlergi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RiwayatAlergi extends Model
+{
+    protected $table = "riwayat_alergi";
+    use HasFactory;
+}
---
+++ app/Models/RiwayatPenyakit.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RiwayatPenyakit extends Model
+{
+    protected $table="riwayat_penyakit";
+    use HasFactory;
+}
